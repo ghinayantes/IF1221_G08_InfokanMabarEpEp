5c7912679d8c7ab3ca1d3eef7ef64e42ba6f4ba9
kethkeey <cathrineangel19@gmail.com>
2026-05-18 00:04:25 +0700

    feat : add output for endGame

--- src/endGame.pl
+++ src/endGame.pl
@@ -101,5 +101,87 @@ cetakPeringkat([data(Nama, Poin, _, _)|Sisa], No) :-
 
 tampilkanRanking(HasilUrut) :-
     cetakPeringkat(HasilUrut, 1).
-  
+
+/* Cetak kartu menjadi format warna-isi */
+cetakKartu(kartu(Warna, angka(N))) :-
+    write(Warna), write('-'), write(N).
+
+cetakKartu(kartu(Warna, skip)) :-
+    write(Warna), write('-skip').
+
+cetakKartu(kartu(Warna, reverse)) :-
+    write(Warna), write('-reverse').
+
+cetakKartu(kartu(Warna, drawTwo)) :-
+    write(Warna), write('-drawTwo').
+
+cetakKartu(kartu(Warna, wild)) :-
+    write(Warna), write('-wild').
+
+cetakKartu(kartu(Warna, wildDrawFour)) :-
+    write(Warna), write('-wildDrawFour').
+
+
+/* Cetak perhitungan poin */
+cetakPerhitungan([], 0).
+
+cetakPerhitungan([K], Total) :-
+    cetakKartu(K),
+    hitungNilaiKartu(K, Nilai),
+    write(' = '),
+    write(Nilai),
+    Total is Nilai.
+
+cetakPerhitungan([K|Sisa], Total) :-
+    cetakKartu(K),
+    hitungNilaiKartu(K, Nilai),
+    write(' = '),
+    write(Nilai),
+    write(' + '),
+
+    cetakPerhitungan(Sisa, TotalSisa),
+    Total is Nilai + TotalSisa.
+
+
+/* Cari pemain dengan kartu habis */
+pemainMenang(Pemain) :-
+    kartuTangan(Pemain, []).
+
+
+/* Tampilkan detail poin tiap pemain */
+tampilkanPoinPemain([]).
+
+tampilkanPoinPemain([P|Sisa]) :-
+    write(P), write(': '),
+    kartuTangan(P, Tangan),
+    (Tangan = []-> write('kartu habis = 0 poin') ;
+        cetakPerhitungan(Tangan, Total),
+        write(' = '),
+        write(Total),
+        write(' poin')),
+    nl,
+    tampilkanPoinPemain(Sisa).
+
+/* END GAME */
+endGame :-
+    pemainMenang(Pemenang),
+    nl,
+    write('Permainan selesai! '),
+    write(Pemenang),
+    write(' menghabiskan semua kartunya!'),
+    nl, nl,
+    write('Berikut perhitungan poin sisa kartu.'), nl,
+
+    urutanPemain(ListPemain),
+    tampilkanPoinPemain(ListPemain),nl,
+    semuaDataPemain(Data),
+    urutkanRanking(Data, HasilUrut),
+
+    write('Urutan pemenang:'), nl,
+    tampilkanRanking(HasilUrut), nl,
+    write('Selamat, '),
+    write(Pemenang),
+    write(' menjadi pemenang!'),
+    nl.
+
 
