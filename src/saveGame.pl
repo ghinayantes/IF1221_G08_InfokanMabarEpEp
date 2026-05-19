a69e38e64fec76f19fdda1bb063c876aabdb3aa4
Sahla Nailah <nailahsahla858@gmail.com>
2026-05-19 18:53:49 +0700

    feat: add new rules and fact

--- src/saveGame.pl
+++ src/saveGame.pl
@@ -5,7 +5,7 @@ saveGame :-
     set_output(Stream),
 
     giliranSekarang(GS),
-    cetakKartu(CK),
+    kartuTeratas(KT),
     warnaAktif(WA),
     arahPermainan(AP),
     urutanPemain(UP),
@@ -13,11 +13,33 @@ saveGame :-
 
     format('urutan_pemain:~q.~n', [UP]),
     format('giliran:~q.~n', [GS]),
-    format('discard_top:~w.~n', [CK]), 
+    write('discard_top:'), cetakKartu(KT), write('.'), nl, 
     format('warna_aktif:~w.~n', [WA]),
     format('arah_permainan:~w.~n', [AP]),
     format('status_UNI:~q.~n', [SU]),
     
+    SemuaKartuPemain(UP).
+
     close(Stream),
     set_output(user_output),
     format('Status permainan berhasil disimpan ke ~w.~n', [NamaFile]).
+
+SemuaKartuPemain([]).
+SemuaKartuPemain([H|T]):-
+    kartuTangan(H, ListKartu),
+    format('kartu(~q):', [H]),
+    write('['),
+    tampilkanKartu(ListKartu),
+    write('].'), nl,
+    SemuaKartuPemain(T).
+
+tampilkanKartu([]).
+tampilkanKartu([H|T]) :- !,
+    cetakKartu(H),
+    tampilkanSisaKartu(T).
+
+tampilkanSisaKartu([]).
+tampilkanSisaKartu([H|T]) :- !,
+    write(','),            
+    cetakKartu(H),
+    tampilkanSisaKartu(T).
