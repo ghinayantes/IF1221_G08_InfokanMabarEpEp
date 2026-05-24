9ecc4111d95ff3b74ebdf357968516ae2c36f690
fromdearest <ghinayantes2006@gmail.com>
2026-05-24 15:20:56 +0700

    feat: add mimic card into list of card

--- src/distribusiKartu.pl
+++ src/distribusiKartu.pl
@@ -55,7 +55,7 @@ buatDeckBaru(Deck) :-
     DaftarWarna = [merah, kuning, hijau, biru],
     DaftarAngka = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
     DaftarAksi = [skip, reverse, drawTwo],
-    DaftarHitam = [wild, wildDrawFour],
+    DaftarHitam = [wild, wildDrawFour, mimic],
     
     kumpulkanSemuaKartu(DaftarWarna, DaftarAngka, DaftarAksi, DaftarHitam, Semua),
     acakList(Semua, Deck).
