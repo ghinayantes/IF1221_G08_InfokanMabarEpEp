75c14c287470a5d3d24bb0482cacad256fb8ade7
fromdearest <ghinayantes2006@gmail.com>
2026-05-29 11:58:36 +0700

    feat: secure state validation and display team layout for tournament mode

--- src/cekInfo.pl
+++ src/cekInfo.pl
@@ -1,11 +1,16 @@
+cekInfo:- 
+    \+ giliranSekarang(_), !,
+    write('Tidak ada permainan yang sedang berjalan. Ketik "start." untuk mulai.'), nl.
 cekInfo:-
     kartuTeratas(KT),
-    urutanPemain(L),
-    nl, 
-    write('Kartu discard top: '), cetakKartu(KT), write('.'), nl,
-    nl,
-    write('Urutan pemain: '), tampilkanUrutan(L), write('.'), nl,
-    nl,
+    urutanPemain(L), nl,
+    write('Kartu discard top: '), cetakKartu(KT), write('.'), nl, nl,
+    % Tampilkan info tim jika mode turnamen
+    (modeTurnamen -> tim1([T1A, T1B]),
+        tim2([T2A, T2B]),
+        format('Tim 1 : ~w, ~w~n', [T1A, T1B]),
+        format('Tim 2 : ~w, ~w~n', [T2A, T2B]), nl ; true),
+    write('Urutan pemain: '), tampilkanUrutan(L), write('.'), nl, nl,
     tampilkanDetailPemain(L, 1).
 
 % Hitung jumlah kartu yang tersembunyi berdasarkan indeks
@@ -18,12 +23,9 @@ hitungIndeksTersembunyi(_, Idx, Total, 0) :-
 hitungIndeksTersembunyi(Pemain, Idx, Total, Jumlah) :-
     Idx =< Total,
     IdxNext is Idx + 1,
-    (kartuTersembunyi(Pemain, Idx) ->
-        hitungIndeksTersembunyi(Pemain, IdxNext, Total, Temp),
-        Jumlah is Temp + 1
-    ;
-        hitungIndeksTersembunyi(Pemain, IdxNext, Total, Jumlah)
-    ).
+    (kartuTersembunyi(Pemain, Idx) -> hitungIndeksTersembunyi(Pemain, IdxNext, Total, Temp),
+        Jumlah is Temp + 1 ;
+        hitungIndeksTersembunyi(Pemain, IdxNext, Total, Jumlah)).
 
 % base case
 tampilkanDetailPemain([], _).
